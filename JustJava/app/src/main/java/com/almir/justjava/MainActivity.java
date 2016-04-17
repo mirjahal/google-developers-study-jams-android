08f4f05d88e5bd912d2526a31313593f4934d2fb
Almir Jr <almirjr.87@gmail.com>
2016-04-17 13:36:45 -0300

    Making an App Interactive

---
+++ JustJava/app/src/main/java/com/almir/justjava/MainActivity.java
@@ -0,0 +1,35 @@
+package com.almir.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int numberOfCoffees = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * 5);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+}
