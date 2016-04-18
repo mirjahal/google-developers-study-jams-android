4d2a69267920c5a1211e523870081daa34b79126
Almir Jr <almirjr.87@gmail.com>
2016-04-17 22:24:42 -0300

    Making an App Interactive

--- JustJava/app/src/main/java/com/almir/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/almir/justjava/MainActivity.java
@@ -9,27 +9,56 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int numberOfCoffees = 2;
+    private int quantity = 3;
+    private static final int COFFEE_PRICE = 5;
+    private TextView quantityTextView;
+    private TextView priceTextView;
+    private TextView totalTextView;
+    private TextView thanksTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayNumberOfCoffees(quantity);
+        displayPrice(COFFEE_PRICE);
     }
 
     public void submitOrder(View view) {
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5);
+        displayTotal(quantity * COFFEE_PRICE);
+        displayThanks();
     }
 
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+    public void increment(View view) {
+        quantity++;
+        displayNumberOfCoffees(quantity);
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    public void decrement(View view) {
+        quantity--;
+        quantity = Math.max(1, quantity);
+        displayNumberOfCoffees(quantity);
+    }
+
+    private void displayNumberOfCoffees(int numberOfCoffees) {
+        quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + numberOfCoffees);
+    }
+
+    private void displayPrice(int price) {
+        priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(price));
+    }
+
+    private void displayTotal(int total) {
+        totalTextView = (TextView) findViewById(R.id.total_text_view);
+        totalTextView.setText("Total: " + NumberFormat.getCurrencyInstance().format(total));
+    }
+
+    private void displayThanks() {
+        thanksTextView = (TextView) findViewById(R.id.thanks_text_view);
+        thanksTextView.setVisibility(View.VISIBLE);
     }
 
 }
